37f77ee9f6f7bb19eba3ead93ce61df4bd64240d
Luba Pecheneva <lp5786766@gmail.com>
2020-06-24 16:15:08 -0400

    Added the file structure

---
+++ config/connection.js
@@ -0,0 +1,6 @@
+// TODO:
+
+// Inside the connection.js file, setup the code to connect Node to MySQL.
+
+
+// Export the connection.
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+-- TODO:
+
+-- In this file, write insert queries to populate the burgers table with at least three entries.
