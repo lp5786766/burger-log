cced7316d5be7dee35844fdf67dcac0498f8bff9
Luba Pecheneva <lp5786766@gmail.com>
2020-07-27 15:59:55 -0400

    get all burgers works/added pictures

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -3,32 +3,27 @@
 // Inside the burgers_controller.js file, import the following:
 
 // Express
-const express = require("express");
+const express = require('express');
 // burger.js
-const burger = require("../models/burger");
-
-
+const burger = require('../models/burger');
 
 // Create the router for the app, and export the router at the end of your file.
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.get("/", (req, res) => {
-
-
-    burger.findAll(function(data) {
-        console.log(res)
-        res.render("index", data);
-      });
-
-
-
-
-   
+router.get('/', (req, res) => {
+  burger
+    .findAll((data) => {
+        const hbsObject = {
+            burgers: data
+          };
+          console.log(hbsObject);
+          res.render("index", hbsObject);
+        console.log(data);
+    });
 });
 
-router.post("/api/burgers/:id", (req, res) => {
-    console.log(res)
+router.post('/api/burgers/:id', (req, res) => {
+  console.log(res);
 });
-
 
 module.exports = router;
